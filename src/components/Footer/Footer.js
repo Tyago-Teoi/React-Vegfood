d1453eed2aa204ca74a3a30adefea7798bbc8507
Tyago Teoi <tyago.teoi@usp.br>
2022-10-31 23:48:46 -0300

    Add footer texts and its styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,18 @@ import './Footer.css'
 function Footer() {
     return(
         <footer className="footer">
-            <img src="/imgs/logo-footer.png" alt="Logo da VegFood" className="footer logo"></img>
+            <img src="/imgs/logo-footer.png" alt="Logo da VegFood" className="logo"></img>
+
+            <div className="footer-text-container">
+                <div className="page-title-container">
+                    <p className="page-title">Sobre Nós</p>
+                    <p className="page-title">Trabalhe Conosco</p>
+                    <p className="page-title">Cadastre seu Restaurante</p>
+                    <p className="page-title">Fale Conosco</p>
+                </div>
+            
+                <p className="copyright">© Copyright 2022 - VegFood</p>
+            </div>
         </footer>
     ) 
     
